eb0caa898929e337719732914e2222797fc4c701
Danil Murzin <danil.murzin.200026@gmail.com>
2024-04-12 14:11:14 +0300

    add lesson02

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let num = 266219;
+let sum = 1;
+num = num.toString().split('');
 
+for (let i = 0; i < num.length; i++) {
+  sum *= +num[i];
+}
+console.log(sum);
+
+sum = sum ** 3;
+console.log(sum);
+
+console.log(sum.toString().slice(0, 2));
