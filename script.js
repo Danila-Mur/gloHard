a47913d048199a4f748b983de363a41d712b18d8
Danil Murzin <danil.murzin.200026@gmail.com>
2024-04-15 13:47:56 +0300

    add lesson03

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-let num = 266219;
-let sum = 1;
-num = num.toString().split('');
+const dayOfWeekRU = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const dayOfWeekEN = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const allDayOfWeek = [dayOfWeekRU, dayOfWeekEN];
 
-for (let i = 0; i < num.length; i++) {
-  sum *= +num[i];
+let lang = 'en';
+
+if (lang === 'en') {
+  console.log(dayOfWeekEN);
+} else if (lang === 'ru') {
+  console.log(dayOfWeekRU);
+} else {
+  console.log('Some wrong...');
 }
-console.log(sum);
 
-sum = sum ** 3;
-console.log(sum);
+switch (lang) {
+  case 'en':
+    console.log(dayOfWeekEN);
+    break;
+  case 'ru':
+    console.log(dayOfWeekRU);
+    break;
+  default:
+    console.log('Some wrong...');
+}
 
-console.log(sum.toString().slice(0, 2));
+lang === 'ru' ? console.log(allDayOfWeek[0]) : console.log(allDayOfWeek[1]);
+
+const namePerson = 'Артем';
+
+namePerson === 'Артем' ? console.log('директор') : console.log('студент');
+namePerson === 'Александр' ? console.log('преподаватель') : console.log('студент');
